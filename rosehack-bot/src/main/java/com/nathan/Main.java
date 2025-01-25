0fce633445dfe3c07de8e0287027c0186398b084
Nathan Obermiller <nobermiller16@gmail.com>
2025-01-25 14:37:06 -0800

    Update Main.java

--- rosehack-bot/src/main/java/com/nathan/Main.java
+++ rosehack-bot/src/main/java/com/nathan/Main.java
@@ -1,8 +1,18 @@
 package com.nathan;
 
-public class Main {
-    public static void main (String[] args) {
-        System.out.println("Test");
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN.token);
+        jdaBuilder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
+        JDA jda = jdaBuilder.build();
+        
+        System.out.println("Hello, World!");
+        jda.addEventListener(new Listeners());
     }
 }
